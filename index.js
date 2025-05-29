c117ea9bcde878184b6fd29789e2f0ade45c1284
Hzygamingtm <zhengyonghoe@gmail.com>
2025-05-29 10:37:14 +0800

    Init Setup

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("helloworld")
+const process = require("node:process");
+const pkg = require("pkg");
+
+class Program {
+    constructor() {
+
+    }
+}
+
+/*
+CMD Line Arguments:
+Username, Domain, Email Addresses, Phone Numbers, IPs, Job Occupation & History
+-u -d -e -p -ip -j -b
+
+*/
